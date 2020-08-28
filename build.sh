bcf543866457173a8254a65fea378284db7c1e93
hasegawa tadamitsu <hasegawa.tadamitsu@gmail.com>
2020-08-29 04:27:56 +0900

    bugfix

--- build.sh
+++ build.sh
@@ -3,12 +3,15 @@
 find . -name \*~ -exec rm -rf {} \;
 rm -rf ./subscribe.tar
 rm -rf ./subscribe_ttrss.xpi
+rm -rf ./subscribe.tar.gz
 
 cd ../
 tar -cvf ./subscribe.tar ./subscribe/
 mv ./subscribe.tar ./subscribe/
 
 cd ./subscribe/
+gzip subscribe.tar
+
 
 cd ./archive
 zip -r ../subscribe_ttrss.xpi *
